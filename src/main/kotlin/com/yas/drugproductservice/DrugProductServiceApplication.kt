7e0ce4756973a006241c85f15bc1fffd928dc1e1
YThilantha <yasitha.thilantha@cambio.lk>
2023-05-19 10:32:23 +0530

    init commit

---
+++ src/main/kotlin/com/yas/drugproductservice/DrugProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.yas.drugproductservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DrugProductServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DrugProductServiceApplication>(*args)
+}
