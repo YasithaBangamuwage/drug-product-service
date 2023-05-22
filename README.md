af9cda2df14abefcc81b8fd9f3e02c40b355c952
YThilantha <yasitha.thilantha@cambio.lk>
2023-05-22 16:04:45 +0530

    use kafka for event service

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # drug-product-service
 
-todo - use spring boot with kotlin and database as couchbase. Create REST APIs to create update delete drug products. At the end make this module as micro service with spring boot. Best if include kafka event handling as well.
+### REST APIS
+
+GET drugs
+http://localhost:8080/api/drugs
+
+GET drug by Id
+http://localhost:8080/api/drugs/1
+
+POST drug
+http://localhost:8080/api/drugs
+
+PATCH drug
+http://localhost:8080/api/drugs
+
+DELETE drug
+http://localhost:8080/api/drugs/1
+
+### How to up Kafka zookeeper in cmd
+
+.\bin\windows\zookeeper-server-start.bat .\config\zookeeper.properties
+
+### How to up kafka server in cmd
+
+.\bin\windows\kafka-server-start.bat .\config\server.properties
+
+### How to read the events in cmd
+
+.\bin\windows\kafka-console-consumer.bat --topic drugProductService --from-beginning --bootstrap-server localhost:9092
--- src/main/kotlin/com/yas/drugproductservice/controller/DrugController.kt
+++ src/main/kotlin/com/yas/drugproductservice/controller/DrugController.kt
@@ -2,8 +2,10 @@ package com.yas.drugproductservice.controller
 
 import com.yas.drugproductservice.model.Drug
 import com.yas.drugproductservice.service.DrugService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,10 +16,14 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
 @RequestMapping("/api/drugs")
-class DrugController(private val service: DrugService) {
+class DrugController(
+    private val service: DrugService,
+    @Autowired private val kafkaTemplate: KafkaTemplate<String, String>
+) {
 
     @ExceptionHandler(NoSuchElementException::class)
     fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
@@ -28,7 +34,11 @@ class DrugController(private val service: DrugService) {
         ResponseEntity(e.message, HttpStatus.BAD_REQUEST)
 
     @GetMapping
-    fun getDrugs(): Collection<Drug> = service.getDrugs()
+    fun getDrugs(): Collection<Drug> {
+
+        kafkaTemplate.send("drugProductService","drugs loading !!")
+        return service.getDrugs()
+    }
 
     @GetMapping("/{drugId}")
     fun getDrug(@PathVariable drugId: String): Drug = service.getDrug(drugId)
@@ -38,9 +48,9 @@ class DrugController(private val service: DrugService) {
     fun addDrug(@RequestBody newDrug: Drug): Drug = service.addDrug(newDrug)
 
     @PatchMapping
-    fun updateDrug(@RequestBody updateDrug : Drug) = service.updateDrug(updateDrug)
+    fun updateDrug(@RequestBody updateDrug: Drug) = service.updateDrug(updateDrug)
 
     @DeleteMapping("/{drugId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun deleteDrug(@PathVariable drugId : String) : Unit = service.deleteDrug(drugId)
+    fun deleteDrug(@PathVariable drugId: String): Unit = service.deleteDrug(drugId)
 }
